3a4b653efac2ae9169bf3b48f13eeab4c3159cde
Syndicate Corperation <45174623+Rxiqi-Development@users.noreply.github.com>
2018-12-12 22:42:04 +0000

    Update server.js

--- test/server.js
+++ test/server.js
@@ -1,17 +1,13 @@
 const express = require('express'),
 app = express(),
 server = require('http').createServer(app),
-io = require('socket.io')(server),
-db = require('quick.db'),
-port = 1337,
-tools = require('./functions.js'),
+port = 1337
+
 Discord = require('discord.js'),
 client = new Discord.Client({
   fetchAllMembers: true
 }),
- discord = require('./discord.js')(client),
-qs = require('qs'),
-session = require('express-session');
+discord = require('./discord.js')(client)
 
 // Listen To Port
 server.listen(port, function() {
@@ -31,16 +27,4 @@ app.get("/new", function(request, response) {
 return response.sendFile(__dirname + '/public/new.html'); // This is gonna be changed as we need to pass in the proper data for this page.
 })
 
-
-app.get("/api", async function(request, response) {
-response.setHeader('Content-Type', 'application/json');
-if (!request.query.key) return response.send('null');
-let data;
-if (request.query.key !== 'all') data = await db.fetch('entries', {
-  target: request.query.key
-});
-else data = await db.fetch('entries');
-if (!request.query.key !== 'all' && data) data.code = qs.parse(data.code).raw;
-response.send(JSON.stringify(data));
-})
 
